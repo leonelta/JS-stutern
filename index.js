d32f31ac8f782efc2a32507a15292ec567ddddf7
leonelta <ketchakaren@gmail.com>
2023-02-12 03:18:22 +0100

    another exercise that consisted of creating multiply variables and display them in a sentence using console.log

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function multiplythem(x, y){
 }
 
 console.log(multiplythem(2,4))
+
+//second exercise
+
+const Firstname = "Karen";
+const Lastname = "Ketcha";
+const track = "frontend development";
+const skills = ["html", "css", "linux"];
+const color = "Black";
+const country = 1960;
+
+const output = `I am called ${Firstname} ${Lastname}. My skills are ${skills}. I belong to the ${track} track. Favorite color is ${color} and my country had its 
+independence on the ${country}`;
+
+console.log(output);
