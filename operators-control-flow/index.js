fa3cacc73c748020054a983c014fa678185f039e
leonelta <ketchakaren@gmail.com>
2023-02-18 21:54:28 +0100

    checking all the condition to see which one matches

--- operators-control-flow/index.js
+++ operators-control-flow/index.js
@@ -1,12 +1,26 @@
+//Auestion 3
+
 const Science_subjects = ["Physics", "Chemistry", "Biology", "technical Drawing"];
 const Arts_subjects = ["Government", "Literature", "Economics", "History"];
 const Social_subjects = ["Accounting", "Commerce", "Marketing", "Geography"];
 const General_subjects = ["English", "Mathematics"];
 
-let Bolatito = class_group;
+
 let class_group = Arts_subjects;
 
 
-if (class_group === Social_subjects){
+if (class_group === Science_subjects){
     console.log(Science_subjects);
+}
+
+else if(class_group === Social_subjects){
+    console.log(Social_subjects);
+}
+
+else if(class_group === Arts_subjects){
+    console.log(Arts_subjects);
+}
+
+else{
+    console.log(General_subjects);
 }
