0400169c525c39db06005e64b2776d1ad8663359
leonelta <ketchakaren@gmail.com>
2023-02-18 19:13:28 +0100

    assigning a value to the variable Bolatito

--- operators-control-flow/index.js
+++ operators-control-flow/index.js
@@ -2,3 +2,8 @@ const Science_subjects = ["Physics", "Chemistry", "Biology", "technical Drawing"
 const Arts_subjects = ["Government", "Literature", "Economics", "History"];
 const Social_subjects = ["Accounting", "Commerce", "Marketing", "Geography"];
 const General_subjects = ["English", "Mathematics"];
+
+let Bolatito = class_group;
+let class_group = Arts_subjects;
+
+
