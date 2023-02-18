f7fc2aaafc658651a2df7acfddcc5124f1678118
leonelta <ketchakaren@gmail.com>
2023-02-18 19:10:23 +0100

    cretaed an array for all the different subjects

---
+++ operators-control-flow/index.js
@@ -0,0 +1,4 @@
+const Science_subjects = ["Physics", "Chemistry", "Biology", "technical Drawing"];
+const Arts_subjects = ["Government", "Literature", "Economics", "History"];
+const Social_subjects = ["Accounting", "Commerce", "Marketing", "Geography"];
+const General_subjects = ["English", "Mathematics"];
