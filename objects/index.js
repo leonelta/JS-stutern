961a97b4330454cc3528d07d32f42b78f8039852
leonelta <ketchakaren@gmail.com>
2023-02-26 13:45:53 +0100

    created a folderobjects which contains an index.js file with soltution to some question

---
+++ objects/index.js
@@ -0,0 +1,11 @@
+//Qustion 1 and 2
+//constructor function
+function  InstagramPost(handle, content, imagelink, views, likes){
+    this.handle = handle;
+    this.content = content;
+    this.imagelink = imagelink;
+    this.views = views;
+    this.likes = likes;
+}
+
+//creating objects
