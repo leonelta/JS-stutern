7203d13b529772a1d1e7f9212b7639e391bebbef
leonelta <ketchakaren@gmail.com>
2023-03-06 03:36:58 +0100

    answering question 1

---
+++ arrays-functions/index.js
@@ -0,0 +1,8 @@
+//Question 1
+
+//mutable array methods can modify the array. Example of mutable array method
+//sort(), pop(), push(), reverse(), splice().
+
+//immutable array method These following methods do not modify the array and return some representation of the array.
+//Examples of immutable array method
+//slice(), join(), filter(), concat(), find().
