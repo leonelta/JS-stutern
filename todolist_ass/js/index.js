9f7567f6c7ed56f78842c6913ab5bcf74f3e830a
leonelta <ketchakaren@gmail.com>
2023-03-15 17:03:41 +0100

    wrote a function displayTodos to be able to display all todo enter

--- todolist_ass/js/index.js
+++ todolist_ass/js/index.js
@@ -39,6 +39,56 @@ function DisplayTodos () {
     todolist.innerHTML = '';
 
     todos.forEach(todo => {
-        
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todoitem')
+
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions= document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+        input.type = 'checkbox';
+        input.checked = todo.done;
+        span.classList.add('donefirst');
+
+        content.classList.add('todocontent');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteButton.classList.add('delete');
+
+        content.innerHTML =  `<input type="text" value="${todo.content}" 
+        readonly>` ;
+        edit.innerHTML ='Edit';
+        deleteButton.innerHTML = 'Delete';
+
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteButton);
+        todoItem.appendChild(label);
+        todoItem.appendChild(content);
+        todoItem.appendChild(actions);
+
+        todolist.appendChild(todoItem);
+
+        if (todo.done) {
+            todoItem.classList.add('done');
+        }
+
+        input.addEventListener('click', encodeURI => {
+            todo.done = encodeURI.target.checked;
+            localStorage.setItem('todos', JSON.stringify(todos));
+
+            if (todo.done) {
+                todoItem.classList.add('done');
+            } else {
+                todoItem.classList.remove('done');
+            }
+
+            DisplayTodos();
+        })
     })
 }
